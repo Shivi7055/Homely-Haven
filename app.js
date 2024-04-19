91e8179af937f87d3965b9ebc72f9bbb72e1fb58
Shivi7055 <post.sk09@gmail.com>
2024-04-19 16:01:12 +0530

    Removed wildcard route

--- app.js
+++ app.js
@@ -83,9 +83,9 @@ app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
  
-app.all("*", (req, res, next) =>{
-    next(new ExpressError(404, "Page not found"));
-});
+// app.all("*", (req, res, next) =>{
+//     next(new ExpressError(404, "Page not found"));
+// });
 
 app.use((err, req, res, next) =>{
     let {statusCode=500, message="Something went wrong"} = err;
