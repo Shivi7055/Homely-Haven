cbafaab90a3593d440c11c58bd3da7a8e59b183e
Shivi7055 <post.sk09@gmail.com>
2024-04-19 16:23:44 +0530

    Re-added wildcard route

--- app.js
+++ app.js
@@ -82,10 +82,10 @@ app.use((req, res, next) =>{
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
- 
-// app.all("*", (req, res, next) =>{
-//     next(new ExpressError(404, "Page not found"));
-// });
+
+app.all("*", (req, res, next) =>{
+    next(new ExpressError(404, "Page not found"));
+});
 
 app.use((err, req, res, next) =>{
     let {statusCode=500, message="Something went wrong"} = err;
