84d3adf1af6761142135a7df861200b26c4c3cb9
Shivi7055 <post.sk09@gmail.com>
2024-04-19 08:58:44 +0530

    Changed secret

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(express.static(path.join(__dirname, "/public")));
 const store = MongoStore.create({
     mongoUrl : dbUrl,
     crypto :{
-        secret : "mysupersecretcode"
+        secret : process.env.SECRET,
     }, 
     touchAfter : 24*3600
 });
@@ -51,7 +51,7 @@ store.on("error", ()=>{
 })
 const sessionOptions = {
     store,
-    secret : "mysupersecretcode",
+    secret : process.env.SECRET,
     resave : false,
     saveUninitialized : true,
     cookie : {
